caf0e44b4b49e56c5a669406c34ba4eb5d6d105d
Nikolaj Due Oesterbye <nikolaj@due-oesterbye.dk>
2017-01-08 00:17:23 +0100

    Add 3G feature
    Add tar.gz rootfs type
    Remove sdcard rootfs type

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ############################################################################
 ##
-## Copyright (C) 2016 Spiri ApS
+## Copyright (C) 2016-2017 Spiri ApS
 ##
 ############################################################################
 
@@ -35,16 +35,29 @@ if [ -f ${PWD}/${BUILDDIRECTORY}/conf/bblayers.conf ]; then
   echo "  \${BSPDIR}/sources/meta-spiri \\" >> ${PWD}/${BUILDDIRECTORY}/conf/bblayers.conf
   echo "  \"" >> ${PWD}/${BUILDDIRECTORY}/conf/bblayers.conf
   echo "" >> ${PWD}/${BUILDDIRECTORY}/conf/bblayers.conf
-
-  cd "${PWD}/${BUILDDIRECTORY}"
-
-  echo "Spiri meta layer has been added to /conf/bblayers.conf"
-  echo ""
-  echo "You can build the Spiri Linux image by typing 'bitbake spiri-image'"
 else
   echo "Error: Could not find bblayers.conf in build directory"
 fi
 
+if [ -f ${PWD}/${BUILDDIRECTORY}/conf/local.conf ]; then
+  echo "" >> ${PWD}/${BUILDDIRECTORY}/conf/local.conf
+  echo "#" >> ${PWD}/${BUILDDIRECTORY}/conf/local.conf
+  echo "# Spiri specific configuration" >> ${PWD}/${BUILDDIRECTORY}/conf/local.conf
+  echo "#" >> ${PWD}/${BUILDDIRECTORY}/conf/local.conf
+  echo "DISTRO_FEATURES_append = \" 3g\"" >> ${PWD}/${BUILDDIRECTORY}/conf/local.conf
+  echo "IMAGE_FSTYPES_remove = \"sdcard\"" >> ${PWD}/${BUILDDIRECTORY}/conf/local.conf
+  echo "IMAGE_FSTYPES_append = \" tar.gz\"" >> ${PWD}/${BUILDDIRECTORY}/conf/local.conf
+  echo "" >> ${PWD}/${BUILDDIRECTORY}/conf/local.conf
+else
+  echo "Error: Could not find local.conf in build directory"
+fi
 
+cd "${PWD}/${BUILDDIRECTORY}"
+
+echo "The build environment has been configured for the Spiri distribution."
+echo ""
+echo "You can build the Spiri Linux image by typing 'bitbake spiri-image' and"
+echo "the Spiri SDK by typing 'bitbake spiri-sdk'"
+echo ""
 unset BUILDDIRECTORY
 
